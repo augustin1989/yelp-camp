afc034c4a18de292469d6534dd46972efbcf8702
Gus <augustin1989@gmail.com>
2017-09-22 18:30:12 -0400

    update app.js with new mongo url

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ var campgroundRoutes = require("./routes/campgrounds");
 var indexRoutes = require("./routes/index");
 
 //mongoose.connect("mongodb://localhost/yelp_camp", {useMongoClient: true});
-mongoose.connect("mongodb://heroku_8jwq1w5d:heroku_8jwq1w5d@ds147884.mlab.com:47884/heroku_8jwq1w5d", {useMongoClient: true});
+mongoose.connect("mongodb://heroku_8jwq1w5d:iu9sq9e2t9qnukt7e0pao2t1nu@ds147884.mlab.com:47884/heroku_8jwq1w5d", {useMongoClient: true});
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
@@ -49,11 +49,11 @@ app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-app.listen(process.env.PORT, process.env.IP, function () {
-   console.log("Server running!");
-});
-
-
-// app.listen(3000, function (req, res) {
+// app.listen(process.env.PORT, process.env.IP, function () {
 //    console.log("Server running!");
 // });
+
+
+app.listen(3000, function (req, res) {
+   console.log("Server running!");
+});
